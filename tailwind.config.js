bd7b5c1f98e80eeaedf9be438b9ca4328a9f3690
Ayush Paul <ayushpaul1111@gmail.com>
2024-02-11 21:55:30 +0530

    -2 tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ const withMT = require("@material-tailwind/react/utils/withMT");
  
 module.exports = withMT({
   content: [
-    `./src/pages/**/*.{js,jsx,ts,tsx}`,
-    `./src/components/**/*.{js,jsx,ts,tsx}`,
+    // `./src/pages/**/*.{js,jsx,ts,tsx}`,
+    // `./src/components/**/*.{js,jsx,ts,tsx}`,
     "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
     "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
